a0828a476e0e2e3fa153ae5242217a0c207bd7c7
kugelest <stefan.kugele@gmail.com>
2022-11-02 16:59:59 +0100

    14

--- kapitel_14/funny_value.c
+++ kapitel_14/funny_value.c
@@ -5,6 +5,6 @@ int main(void) {
     int *pInt = (int *) malloc(100 * sizeof(int));
     pInt[99] = 0;
     printf("pInt: %p\n", (void *) pInt);
-    // free(pInt);
+    free(pInt);
     free(&pInt[1]);
 }
