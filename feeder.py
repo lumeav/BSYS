d31ad297e7bc1357059c825fa470a6572db23f37
lumeav <lukas.mertke@htwg-konstanz.de>
2023-01-22 21:47:24 +0100

    feeder

--- feeder.py
+++ feeder.py
@@ -104,20 +104,85 @@ def in_the_end():
         print("Lifetime:%f" % sum_lifetime)
 
 
+def getShortestEatingTime(eating, animal):
+    min = 100000        # just a big number
+    for i in range(0, len(eating)):
+        t = eating[i].get_remaining_status_times_s()[eating[i].get_status()]
+        if t < min:
+            min = t
+            animal = eating[i]
+    return animal
+
+
+def orderInto(que, animal):
+    t = animal.get_remaining_status_times_s()[animal.get_status()]
+    if (len(que) < 1):
+        que.append(animal)
+    else:
+        j = 0
+        for j in range(0, len(que)):
+            que[j].wakeup()
+            if(t < que[j].get_remaining_status_times_s()[que[j].get_status()]):
+                break
+        que.insert(j, animal)
+
+#ORDER INTO STRAV_QUE BY LIFETIME (ARRIVAL TIME)
+def orderInto_fcfs(que, animal):
+    t = animal.get_lifetime()
+    if (len(que) < 1):
+        que.append(animal)
+    else:
+        j = 0
+        while (t < que[j].get_lifetime()):
+            j = j + 1
+            if (j == len(que)):
+                break
+        que.insert(j, animal)
+
+
+def animalWouldKill(eating, animal):
+        if (getattr(animal, 'ANIMAL_TYPE') == 1):  # If Cat
+            for j in range(0, len(eating)):
+                if (getattr(eating[j], 'ANIMAL_TYPE') == 0):    # Don't send if mouse is there
+                    return True
+                elif (j == len(eating) - 1):
+                    return False
+        elif (getattr(animal, 'ANIMAL_TYPE') == 2):  # If Dog
+            for j in range(0, len(eating)):
+                if (getattr(eating[j], 'ANIMAL_TYPE') == 1):    # Don't send if cat is there
+                    return True
+                elif (j == len(eating) - 1):
+                    return False
+        else:  # If Mouse
+            return False
+
+
 def main():
+
+    ### SORT ANIMALS ASCENDING TO THEIR SATISFIED TIME AT BEGINNING
 
     animal = [dogs,mouse,cats]
     animallist = []
+    animallist_fcfs = []
+    numDishes = 0
+    if (len(dogs) > 0):
+        numDishes = len(dogs[0].dishes)
+    elif (len(cats) > 0):
+        numDishes = len(cats[0].dishes)
+    elif (len(mouse) > 0):
+        numDishes = len(mouse[0].dishes)
 
     sat_que = collections.deque()
     strav_que = collections.deque()
+    eating = collections.deque()
 
     for a in animal:
         for ani in a:
             animallist.append(ani)
-
+            #animallist_fcfs.append(ani) #FCFS NECESSARY
 
     size = len(animallist)
+    #size = len(animallist_fcfs) #FCFS NECESSARY
 
     for i in range(size):
         for j in range(i):
@@ -125,51 +190,98 @@ def main():
                 tmp = animallist[j]
                 animallist[j] = animallist[j+1]
                 animallist[j+1] = tmp
+    
+    # FOR LOOP ORDERING ANIMALS BY LIFETIME (ARRIVAL TIME) FCFS NECESSARY
+
+    # for i in range(size):
+    #     for j in range(i):
+    #        if (animallist_fcfs[j].get_lifetime() > animallist_fcfs[j+1].get_lifetime()):
+    #             tmp = animallist_fcfs[j]
+    #             animallist_fcfs[j] = animallist_fcfs[j+1]
+    #             animallist_fcfs[j+1] = tmp
+    
 
     for a in animallist:
         sat_que.append(a)
+    
+    # for a in reversed(animallist_fcfs): #FCFS NECESSARY
+    #     sat_que.append(a) #FCFS NECESSARY
 
+    ### FEEDING LOOP
 
-    for i in range(0, 200):
+    for i in range(0, 100):
+        ### FETCH FROM STARVING QUEUE IF NOT EMPTY
         if len(strav_que) > 0:
-            animal = strav_que.popleft()
-            animal.goto_dishes()
-        elif len(sat_que) > 0:
-            animal = sat_que.popleft()
-            animal.goto_dishes()
-
-        animal.wakeup()
-        time.sleep(0.1)
-
-        removement = []
-
-        for a in sat_que:
-            if (State(a.get_status()) == State.STRAVING):
-                removement.append(a)
-                if (len(strav_que) < 1):
-                    strav_que.append(a)
+            animal = strav_que[0]              # GET MOST STARVING ANIMAL
+            kill = False
+            for x in range(0, len(strav_que)):
+                animal = strav_que[x]
+                if not (animalWouldKill(eating, animal)):
+                    kill = False
+                    break
                 else:
-                    j = 0
-                    t2 = a.get_remaining_status_times_s()[a.get_status()]
-                    while (t2 > strav_que[j].get_remaining_status_times_s()[strav_que[j].get_status()]):
-                        j = j + 1
-                    strav_que.insert(j, a)
-
-        for r in removement:
-            sat_que.remove(r)
-
-        if (State(animal.get_status()) == State.SATISFIED):
-            t = animal.get_remaining_status_times_s()[animal.get_status()]
-            if (len(sat_que) < 1):
-                sat_que.append(animal)
-            else:
-                j = 0
-                while (t > sat_que[j].get_remaining_status_times_s()[sat_que[j].get_status()]):
-                    j = j + 1
-                sat_que.insert(j, animal)
+                    kill = True
 
 
+            if (not kill and len(eating) < numDishes):   # ANIMAL WOULDN'T KILL
+                strav_que.remove(animal)
+                if(animal.get_status() != State.DEAD):
+                    rt = animal.goto_dishes()
+                    if(not rt):
+                        strav_que.appendleft(animal)
+                    else:                                           # SEND ANIMAL TO DISH
+                        orderInto(eating, animal)               # ADD EATING ANIMAL
+                        #orderInto_fcfs(eating, animal) #FCFS NECESSARY
+                        for a in eating:
+                            with a.cv_lock:
+                                while (State(a.get_status()) != State.EATING and State(a.get_status()) != State.DEAD):
+                                    a.wakeup()
+                                    a.condition.wait()
 
+
+            if (len(eating) > 0):
+                    eater = eating[0]      # GET SHORTEST REMAINING-EATING-TIME ANIMAL
+                    if (len(eating) == numDishes):                     # JUST WAIT IF DISHES ARE FULL
+                        eater.cv_lock.acquire()
+                        while (eater.get_status() == State.EATING and State(animal.get_status()) != State.DEAD):    # WAIT WHILE ANIMAL EATING
+                            eater.condition.wait()                     # WAKEUP SELF WHEN STATUS CHANGED
+                        eater.cv_lock.release()
+                        # PUT SATISFIED ANIMAL BACK INTO SATISFIED QUEUE
+                        if (State(eater.get_status()) == State.SATISFIED):
+                            eating.remove(eater)                           # REMOVE (not anymore) EATING ANIMAL
+                            orderInto(sat_que, eater)
+                            #sat_que.append(eater) #FCFS NECESSARY
+
+                    removement = []
+                    for a in eating:
+                        with a.cv_lock:
+                            if a.get_status() != State.EATING:
+                                removement.append(a)
+                                if a.get_status() != State.DEAD:
+                                    if a.get_status() == State.SATISFIED:
+                                        orderInto(sat_que, a)
+                                        #sat_que.append(a) #FCFS NECESSARY
+                                    elif a.get_status() == State.STRAVING:
+                                        orderInto(strav_que, a)
+                                        #orderInto_fcfs(strav_que, a) #FCFS NECESSARY
+                    for r in removement:
+                        eating.remove(r)
+
+
+        ### CHECK IF AN ANIMAL IS STARVING
+
+        if len(sat_que) > 0:
+
+            a = sat_que.popleft()
+
+            a.cv_lock.acquire()
+            while (a.get_status() != State.STRAVING and a.get_status() != State.DEAD):      # WAIT WHILE SATISFIED
+                a.wakeup()
+                a.condition.wait()                          # WAKEUP WHEN STATUS CHANGED
+            if (a.get_status() == State.STRAVING):
+                orderInto(strav_que, a)
+                #orderInto_fcfs(strav_que, a)
+            a.cv_lock.release()
 
 
 
